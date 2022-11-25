b93807ec060ef143059a26d695d8d1f69f37e081
lcaei <swiinnocent@gmail.com>
2022-11-25 04:18:20 +0100

    Add files via upload

---
+++ CamLodge/src/components/Search.js
@@ -0,0 +1,14 @@
+const Search = () => {
+    return (
+        <header>
+            <h2 className="header__title">Search it. Explore it. Buy it.</h2>
+            <input
+                type="text"
+                className="header__search"
+                placeholder="Enter an region, city or Quarter"
+            />
+        </header>
+    );
+}
+
+export default Search;
